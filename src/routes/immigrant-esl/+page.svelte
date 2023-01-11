968a699a4dbb3d3bb21a83f3635a581a63252672
Chad <chadananda@gmail.com>
2023-01-11 13:58:05 -0700

    updated routes to support multiple pages

---
+++ src/routes/immigrant-esl/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+  import "../../app.css";
+</script>
+
+<svelte:head>
+
+  <title>WholeReader - A Path to Fluent English</title>
+
+  <meta name="description" content="WholeReader is a reading app that can help you acquire the high-quality English you need to succeed in the US. Its read-along books make learning English fun and easy.">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</svelte:head>
+
+
+<header class="bg-gray-300 py-2 px-8 mb-4 text-center text-2xl font-serif overflow-x-hidden w-full roboto">
+<h1 class="text-3xl pb-2 roboto ">WholeReader - Your Key to Fluent English</h1>
+<h2 class="text-sm sm:text-xl max-w-screen-md mx-auto">
+ Reading is one of the most effective ways to acquire new languages, WholeReader will make it easy and fun
+</h2>
+<h3 class="text-sm sm:text-xl max-w-screen-md mx-auto">
+ <em>For Spanish-speaking Adults</em>
+</h3>
+</header>
+
+
+<main class="container p-2 overflow-x-hidden mx-1 sm:mx-auto sm:max-w-screen-md   ">
+
+<img class="float-right m-1 w-36 hidden sm:block h-auto" width="244" height="512"  src="/img/phone-sshot.webp" alt="WholeReader screenshot on a phone" />
+<img class="m-0 mb-2 -mt-2 w-screen block sm:hidden h-auto" width="619" height="285.2" src="/img/book-cover-h.webp" alt="WholeReader screenshot on a phone" />
+
+<p class="personal-open text-base font-sans mt-5">Welcome Spanish speakers, if you are looking to improve your English and have more opportunities in the United States, WholeReader is the perfect solution for you. With our read-along books, you will be able to acquire the high-quality English you need to succeed in the US. </p>
+
+<p class="personal-note text-base font-sans italic">We believe that reading is one of the most effective ways to acquire new languages, and with WholeReader, it will be easy and fun. We have selected a wide range of books that will help you improve your grammar, vocabulary and comprehension, and our app will read the text to you, so you can focus on understanding the meaning of the words.
+</p>
+
+<p class="call-to-action clear">To get WholeReader free, use this coupon link: <br>
+<a class="" href="https://wholereader.com/link/Gzgfc">https://wholereader.com/link/Gzgfc</a>
+</p>
+
+<p class="italic">The benefits of reading are endless, it will help you:</p>
+
+<ul>
+  <li>Improve your vocabulary and grammar</li>
+  <li>Enhance your comprehension and understanding of the language</li>
+  <li>Gain confidence when speaking and writing in English</li>
+  <li>Prepare for language tests, like TOEFL, TOEIC, and IELTS</li>
+  <li>Understand native English speakers better and communicate more effectively</li>
+</ul>
+
+<p>Don't let language be a barrier to your success. Take the first step towards fluency with WholeReader today. <a class="" href="https://wholereader.com/link/Gzgfc">https://wholereader.com/link/Gzgfc</a></p>
+
+</main>
+
+
