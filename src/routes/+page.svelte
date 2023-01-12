9eadc12e55a8761acf0a814f4b8c152bb3c9a165
Chad <chadananda@gmail.com>
2023-01-11 18:41:31 -0700

    added temporary home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <svelte:head>
-    <!-- redirect brower to https://wholereader.com -->
-    <meta http-equiv="refresh" content="1; URL=https://wholereader.com/about" />
+    <!-- <meta http-equiv="refresh" content="1; URL=https://wholereader.com/about" /> -->
 </svelte:head>
 
-<div style="color: gray; margin: auto; text-align: center; margin-top:5em;">redirecting...</div>
+<!-- <div style="color: gray; margin: auto; text-align: center; margin-top:5em;">redirecting...</div> -->
+
+
+<ul style="margin:2em; font-size: 150%; line-height:1.5em">
+    <li><a href="/homeschool">homeschool</a></li>
+    <li><a href="/immigrant-esl">immigrant-esl</a></li>
+    <li><a href="/immigrant-esl-en">immigrant-esl-en</a></li>
+    <li><a href="/korea-esl">korea-esl</a></li>
+    <li><a href="/korea-esl">korea-esl-en</a></li>
+</ul>
 
 
 
